3f48d4069d12fe1879b399658e253ae8b5d4b9f1
Laptop\Piratepoots <piratepoots@gmail.com>
2018-01-07 14:43:30 -0600

    Added code to Position Report to check in on the console

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h" // as of 4.17, IWYU (include what you use) was introduced so now we need to include header files that we need.
 
 
 // Sets default values for this component's properties
@@ -19,7 +20,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();  // "->" instead of "." because this (GetOwner) is a pointer.
+	UE_LOG(LogTemp, Warning, TEXT("PositionReport reporting for %s!"), *ObjectName); // Prints log to the console (Output log).  This Warning Log is telling what item is reporting in.
 	
 }
 
